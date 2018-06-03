16989f1eceae7e977a2da002f0fa21d3569dd729
wowbob396 <wowbob396@gmail.com>
2018-06-03 13:48:25 -0500

    added basic sidebar nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,40 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
+import { RouterModule, Routes } from '@angular/router';
 
+import { HomePageComponent } from './home-page/home-page.component';
+import { EditorPageComponent } from './editor-page/editor-page.component';
+import { SettingsPageComponent } from './settings-page/settings-page.component';
+
+const appRoutes: Routes = [
+  {path: 'home-page', component: HomePageComponent},
+  {path: 'editor-page', component: EditorPageComponent},
+  {path: 'settings-page', component: SettingsPageComponent}
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    HomePageComponent,
+    EditorPageComponent,
+    SettingsPageComponent,
+    NavBarComponent
   ],
   imports: [
-    BrowserModule
+    RouterModule.forRoot(appRoutes),
+    BrowserModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
